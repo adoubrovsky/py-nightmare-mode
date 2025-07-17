09f6fcafbb3875f9ee67d2c9e9f74dae3690005b
Alyssa Doubrovsky <adoubrovsky@gmail.com>
2025-07-16 17:13:06 -0700

    File Restructuring
    Removed encompassing file to easily clone into mod folder

---
+++ data-updates.lua
@@ -0,0 +1,11 @@
+for i, item in pairs(data.raw.item) do
+    if item.fuel_category ~= nil and item.fuel_category == "biomass" then
+        data.raw.item[item.name].burnt_result = "wood-ash"
+    end
+end
+
+data.raw.recipe["clinkers-processing-2"]:add_unlock("coal-mk01")
+data.raw.recipe["clinkers-processing-3"]:add_unlock("coke-mk02")
+data.raw.recipe["calcinate-separation"]:remove_unlock("fluid-separation")
+data.raw.recipe["calcinate-separation"]:add_unlock("ash-separation")
+data.raw.recipe["wood-ash-separation"]:add_unlock("ash-separation")
